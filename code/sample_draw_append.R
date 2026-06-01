7f4eda158e4e550c8c9e04f9a20fdf07103f052c
smccord <13965706+smccord@users.noreply.github.com>
2026-06-01 14:58:13 -0600

    Add AIM 2025

--- code/sample_draw_append.R
+++ code/sample_draw_append.R
@@ -1,5 +1,5 @@
 # SELECTING AN SPATIALLY BALANCED SAMPLE FOR FRACTIONAL COVER MODEL VALIDATION #
-# 2024-09-23
+# 2026-05-04
 
 # This will produce a spatially-balanced sampling design made of a subset of the
 # available BLM AIM and LMF points from years in which at least 100 points were
@@ -16,7 +16,7 @@ projection_aea <- sp::CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=
 # The path to the incoming data. This is necessary because some LMF data have
 # been ingested into a GDB but not added to the national database yet
 # Change this to a full path if data isn't a subfolder in your working directory
-data_path <- "~/Projects/AIM/Data/AIMTerrestrialPub1-15-26.gdb/AIMTerrestrialPub1-15-26.gdb/"
+data_path <- "~/Projects/AIM/Data/AIM_Terrestrial_Pub_4-8-26.gdb/AIM_Terrestrial_Pub_4-8-26.gdb"
 
 # The path to write the sampling design to once this is all complete
 # Change this to a full path if output isn't a subfolder in your working
@@ -43,9 +43,9 @@ all_points <- sf::st_transform(dplyr::bind_rows(all_other_lmf,all_other_aim),
 
 # Again, the sampling design needs to be stratified by year
 # This will extract the year from the DateVisited variable
-all_points <- all_points |>
+all_points_sample <- all_points |>
   dplyr::mutate(DateVisited = as.POSIXct(DateVisited, format = "%Y/%m/%d %H:%M", tz = "UTC")) |>
-  dplyr::mutate(YearVisited = lubridate::year(DateVisited))|> subset(YearVisited==2024)
+  dplyr::mutate(YearVisited = lubridate::year(DateVisited))|> subset(YearVisited==2025)
 
 
 #### DESIGN ####################################################################
@@ -56,11 +56,11 @@ all_points <- all_points |>
 # The easiest way to get this is to use table() which will provide a named count
 # of points associated with each value in the variable year then multiply those
 # counts by 0.1 to get the desired sample sizes
-sample_size_vector <- ceiling(table(all_points$YearVisited) * 0.1)
+sample_size_vector <- ceiling(table(all_points_sample$YearVisited) * 0.1)
 
 # Only years in which there were at least 100 points sampled should be included
 # in the sampling design
-sample_size_vector <- sample_size_vector[table(all_points$YearVisited) >= 100]
+sample_size_vector <- sample_size_vector[table(all_points_sample$YearVisited) >= 100]
 
 # Set a seed numebr so that this is reproducible
 set.seed(46290)
@@ -69,13 +69,13 @@ set.seed(46290)
 # filtered to only include those corresponding to qualifying years
 # Because this isn't a design in which there might be later rejections, only
 # "base" points are being drawn
-sample <- spsurvey::grts(sframe = dplyr::filter(all_points,
+sample <- spsurvey::grts(sframe = dplyr::filter(all_points_sample,
                                                 YearVisited %in% names(sample_size_vector)),
                          n_base = sample_size_vector,
                          stratum_var = "YearVisited")
 
-# Add in the other NRI points
-old_aim <- sf::st_read("output/AIM_reserve_validataion_2024-09-23.csv") |>
+# Add in the validation points from previous samples
+old_aim <- sf::st_read("output/AIM_reserve_validation_2026-01-29.csv") |>
  dplyr::mutate(YearVisited = as.numeric(YearVisited))
 
 sample_points <- dplyr::bind_rows(dplyr::select(.data = sample$sites_base,
@@ -84,6 +84,10 @@ sample_points <- dplyr::bind_rows(dplyr::select(.data = sample$sites_base,
                                   dplyr::select(.data = old_aim,
                                                 PrimaryKey,
                                                 YearVisited))
+
+# add Lat/Long
+sample_points <- dplyr::left_join(sample_points, all_points |>
+                                    dplyr::select(PrimaryKey, Latitude_NAD83, Longitude_NAD83) |> sf::st_drop_geometry())
 
 
 #### WRITING ###################################################################
